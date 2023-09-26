74bf8ba4a9b97cb8073a6ff498a60f98c76117a8
emilvn <emilvnielsen@hotmail.com>
2023-09-26 19:09:14 +0200

    tabs converted to class

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import {initTabs} from "./src/view/tabs.js";
+import {TabUtility} from "./src/view/TabUtility.js";
 import {View} from "./src/view/View.js";
 import {DataService} from "./src/services/DataService.js";
 
 window.addEventListener("load", main);
 export let members;
 async function main() {
-	initTabs();
+	TabUtility.initTabs();
 	members = await DataService.getMembers();
 	const results = await DataService.getResults();
 	View.displayMembers(members);
